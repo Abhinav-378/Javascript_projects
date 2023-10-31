d71c231517b7cb3db6830c73dc47e81e068c3be7
Abhinav Choukse <124260348+Abhinav-378@users.noreply.github.com>
2023-10-31 20:51:36 +0530

    Add files via upload

---
+++ Error Handling And prototypes/06_StaticMethodCalc.js
@@ -0,0 +1,11 @@
+class add{
+    n1;
+    n2;
+    constructor(a,b){
+        this.n1 = a;
+        this.n2 = b;
+        console.log(`Addition: ${this.n1 + this.n2}`);
+    }
+}
+
+const sum = new add(3,6);
